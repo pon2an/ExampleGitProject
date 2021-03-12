cb61a45898e0d385145e96e240075583d15eb2e5
pon2an <78740700+pon2an@users.noreply.github.com>
2021-03-12 22:25:20 +0700

    Homework3Anan63120501020
    งานนนนนนนน

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace homework63120501020Anan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input mode : ");
+            string mode = Console.ReadLine();
+            Console.Write("Please input number x : ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Please input number y : ");
+            double y = double.Parse(Console.ReadLine());
+
+            double Time = Math.Pow(x - 1, 2);
+            double Price = Math.Sqrt(y) + 1;
+
+            if (x > 0)
+            {
+                switch (mode)
+                
+                 {case "time":
+                      Console.WriteLine("time = " + x + " , " + Time);
+                  break;
+                  case "price":
+                      Console.WriteLine("price = " + Price + "," + y);
+                  break;
+                  default:
+                      Console.WriteLine("Invalid mode");
+                  break; }
+                
+            }
+            else { Console.WriteLine("Invalid mouse position"); }
+
+            Console.ReadLine();
+        }
+    }
+}
