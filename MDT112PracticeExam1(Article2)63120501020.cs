435af8c55b82908dac9c93a75f533594e0e17b1a
pon2an <78740700+pon2an@users.noreply.github.com>
2021-03-28 16:58:08 +0700

    MDT112PracticeExam1(Article2)63120501020
    PracticeExam1(Article2)PracticeExam1(Article2)63120501020AnanSae-tiew

---
+++ MDT112PracticeExam1(Article2)63120501020.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MDT112PracticeExam1_2_63120501020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Number a : ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Input Number b : ");
+            int b = int.Parse(Console.ReadLine());
+
+            int x = a;
+            int y = b;
+
+            while (x != y) 
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine("x = "+x);
+            Console.ReadLine();
+        }
+    }
+}
