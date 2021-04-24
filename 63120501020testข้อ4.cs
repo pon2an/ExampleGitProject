597744cae0735284ac23daabd45e52fa2fe1e7ad
pon2an <78740700+pon2an@users.noreply.github.com>
2021-04-24 15:33:50 +0700

    63120501020testข้อ4
    ข้อ4

---
+++ 63120501020testข้อ4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace testข้อ._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y, dx, dy, step;
+            Console.WriteLine("input float x1 :");
+            float x1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("input float x2 :");
+            float x2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("input float y1 :");
+            float y1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("input float y2 :");
+            float y2 = float.Parse(Console.ReadLine());
+
+            dx = x2 - x1;
+            dy = y2 - y1;
+
+            if (Math.Abs(dx) >= Math.Abs(dy))
+            {
+                step = Math.Abs(dx);
+                dx /= step; dy /= step; x = x1; y = y1;
+                for (int i = 1; i <= step; i++)
+                {
+                    Console.WriteLine(x+y);
+                    x += dx;
+                    y += dy;
+                }
+                Console.WriteLine("End");
+            }
+            else
+            {
+                step = Math.Abs(dy);
+                dx /= step; dy /= step; x = x1; y = y1;
+                for (int i = 1; i <= step; i++)
+                {
+                    Console.WriteLine(x+y);
+                    x += dx;
+                    y += dy;
+                }
+                Console.WriteLine("End");
+            }
+        }
+    }
+}
