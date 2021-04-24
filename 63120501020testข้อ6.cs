159cb7f32acc68be4aa9170ae7d7fadaa4574604
pon2an <78740700+pon2an@users.noreply.github.com>
2021-04-24 15:50:03 +0700

    63120501020testข้อ6

---
+++ 63120501020testข้อ6.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ข้อ._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input char : ");
+            char CHAR = char.Parse(Console.ReadLine());
+            Console.Write("input number (Step width) : ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("input number (Step height) : ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("input number (Number of steps) : ");
+            int c = int.Parse(Console.ReadLine());
+
+
+            for (int x = 1; x <= c; x++)
+            {
+                for (int y = 1; y <= b; y++)
+                {
+                    for (int z = 1; z <= a; z++)
+                        Console.Write(CHAR);
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
