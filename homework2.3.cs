4dff69bca0367e1afdc2b04bf95b03cdabd21a8d
pon2an <78740700+pon2an@users.noreply.github.com>
2021-06-12 22:11:17 +0700

    homework2.3VAT%cal
    HW

---
+++ homework2.3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input PRICE : ");
+            double A = Convert.ToDouble(Console.ReadLine());
+            Console.Write("input VAT% : "); //Thai VAT 7%
+            double B = Convert.ToDouble(Console.ReadLine());
+            double C = A + (A * B / 100);
+            Console.WriteLine("NET PRICE : " + C);
+            Console.ReadLine();
+        }
+    }
+}
