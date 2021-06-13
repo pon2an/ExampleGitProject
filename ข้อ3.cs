b7d7b93f8f164c76779a951405a55b560ed53536
pon2an <78740700+pon2an@users.noreply.github.com>
2021-06-13 17:30:23 +0700

    ข้อ3

---
+++ ข้อ3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _333
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//ไม่รู้ววววววว อ้ากกกกกกก ทำไม่ทันนนน
+            Console.Write("input your number : ");
+            string num = Console.ReadLine();
+            Type[] numarray = new Type[9999999];
+            do
+            {   Console.Write("number : ");
+                num = Console.ReadLine();} 
+            while (num != "End");
+
+            if (num == "End")
+            {   Console.WriteLine(" ");
+                string f = Console.ReadLine();}
+
+            Console.WriteLine(Math.Max(num));
+            Console.WriteLine(Math.Min(num));
+
+            //Console.Write("input your number (x) : ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.Write("input your number (y) : ");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("");
+
+            //Console.Write("Maximum number : ");
+            //Console.WriteLine(Math.Max(num1, num2));
+            //Console.Write("Minimum number : ");
+            //Console.WriteLine(Math.Min(num1, num2));
+        }
+        static void Numinfo(double AllNow, int ABCD) { }
+
+    }
+}
