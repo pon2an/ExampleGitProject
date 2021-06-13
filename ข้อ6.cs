fafbc064570be0a7e1921a0f6276fa494bcd7165
pon2an <78740700+pon2an@users.noreply.github.com>
2021-06-13 17:07:14 +0700

    สอบข้อ6
    63120501020

---
+++ ข้อ6.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+namespace _6666
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//ได้โปรดเมตตาด้วย i will daed
+            Console.WriteLine(" Please input a number from 0 - 3 to choose the following menu : ");
+            Console.WriteLine(" [0] Show all recent files ");
+            Console.WriteLine(" [1] Open new file ");
+            Console.WriteLine(" [2] Open recent file ");
+            Console.WriteLine(" [3] Exit ");
+
+            Console.Write("Your select: ");
+            string choice = Console.ReadLine();
+
+            StreamWriter file = null;
+            file = null;
+            string fileName = @"TextFile1.txt";
+            string line = null;
+
+            Console.Write("Enter your message: ");
+            line = Console.ReadLine();
+            file.WriteLine(line);
+            try
+            {
+                if (choice == "0")
+                { file = new StreamWriter(fileName); }
+                else if (choice == "1")
+                { file = File.AppendText(fileName); }
+                else if (choice == "2")
+                { file = File.CreateText(fileName); }
+                else if (choice == "3")
+                { file.Close(); }
+                else
+                { Console.WriteLine(" Invalid menu number."); }
+
+                file.WriteLine(line);
+            }
+
+            catch (FileNotFoundException e)
+            {   Console.WriteLine("“No recent file.”"); }
+
+            finally
+            {
+                if (file != null)
+                { file.Close(); }
+            }
+
+            file.Close();
+
+        }
+    }
+}
