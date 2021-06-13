23bd3ef0cb7420d67d71f4d4a60b51c2ee41432d
pon2an <78740700+pon2an@users.noreply.github.com>
2021-06-13 17:27:07 +0700

    ข้อ1
    63120501020

---
+++ ข้อ1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _11111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            Console.WriteLine("input เลขมา (NUM!!!)");
+            int.Parse(Console.ReadLine());
+            int[,] numarray = new int[3,3];
+            for (i = 0; i < 3; i++) ;
+            {
+                for (i = 0; i < 3; i++) ;
+                {
+                    numarray[j, i] = char.Parse(Console.ReadLine());
+                }
+            }
+            for (i = 0; i < 3; i++) ;
+            {
+                for (i = 0; i < 3; i++) ;
+                {
+                    numarray[j, i] = char.Parse(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
