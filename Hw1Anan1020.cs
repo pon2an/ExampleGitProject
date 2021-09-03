7c1cd780dcd80bb05ed280c554bdc44d3c9efb4c
pon2an <78740700+pon2an@users.noreply.github.com>
2021-09-03 22:40:39 +0700

    CodeHw1Anan1020
    CodeHw1Anan1020
    ใช้คอมม.แล้วมันทำช้าครับ ทั้งเขียน รันดูโค้ด แต่ละที 5-10นาทีกว่าจะมาแต่ละที ขนาดเปิดคอมหรือเปิดกูเกิ้ลส่ง github ยังดีเลย์นานมากครับ พอมีวิธีช่วยเหลืออะไรมั้ยครับ แบบส่งเป็นไฟล์text. แทน หรืออะไรก็ได้ครับ ไม่ไหวจริงๆครับ

---
+++ Hw1Anan1020.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+namespace HW1ClothesShopSupport
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User jame = new User("jame watson", "jame@gmail.com");
+            Tshirt shirt1 = new Tshirt("red", "L", "Monitor lizard", 500);
+            Tshirt shirt2 = new Tshirt("black", "M", "FishStop", 750);
+            Tshirt shirt3 = new Tshirt("rainbow", "S", "Prayut Chan-O-cha", 625);
+            Address Ajame = new Address("Thanon Phutthamonthon", "Nakon Prathom", "10180");
+
+            jame.Sent(); jame.shirt(shirt1); jame.shirt(shirt2); jame.shirt(shirt3);
+
+            shirt1.OptionalTshirt(); shirt2.OptionalTshirt(); shirt3.OptionalTshirt();
+
+            jame.Address(Ajame); Ajame.OptionalAddress();
+
+            //Console.WriteLine("total cost : {0} bath", shirt1.Price + shirt2.Price + shirt3.Price);
+            jame.TotalCost();
+            Console.ReadLine();
+        }
+
+    }
+
+    class User
+    {
+        public string UserName, @Email;
+
+        public List<Tshirt> OrderTShirst; public List<Address> Location;
+
+        public User(string name, string email)
+        {
+            UserName = name; @Email = email;
+            OrderTShirst = new List<Tshirt>();
+            Location = new List<Address>();
+        }
+
+        public void shirt(Tshirt tshirt)
+        { OrderTShirst.Add(tshirt); }
+        public void Address(Address Address)
+        { Location.Add(Address); }
+        public void Sent()
+        {
+            Console.WriteLine("Username : " + this.UserName);
+            Console.WriteLine("Email : " + this.@Email);
+            Console.WriteLine();
+        }
+
+        public void TotalCost()
+        {
+            float total = 0;
+            foreach (Tshirt tshirt in OrderTShirst)
+            { total = total + tshirt.Price; }
+            Console.WriteLine("Total cost : " + total + " Bath");
+        }
+    }
+
+    class Tshirt
+    {
+        public string Size, Color, Image; public float Price;
+
+        public Tshirt(string color, string size, string image, float priec)
+        { Size = size; Color = color; Price = priec; Image = image; }
+
+        public void OptionalTshirt()
+        {
+            Console.WriteLine("**********************");
+            Console.WriteLine("Size : " + this.Size);
+            Console.WriteLine("Color : " + this.Color);
+            Console.WriteLine("Priec : " + this.Price);
+            Console.WriteLine("Image : " + this.Image);
+            Console.WriteLine("**********************");
+            Console.WriteLine();
+
+        }
+    }
+
+    class Address
+    {
+        public string Street, City, Zipcode;
+
+        public Address(string street, string city, string zipcode)
+        { Street = street; City = city; Zipcode = zipcode; }
+
+        public void OptionalAddress()
+        { Console.WriteLine("131/75 street : " + this.Street + " ,city : " + this.City + " ,zipcode : " + this.Zipcode); }
+    }
+
+    class ShoppingCart
+    {
+        public List<Tshirt> tshirt; public List<Address> addresses;
+
+        public ShoppingCart()
+        { tshirt = new List<Tshirt>(); addresses = new List<Address>(); }
+    }
+}
