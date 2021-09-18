6e50241ff2a88f29a5b24adcf4a6e14e8f8d3841
pon2an <78740700+pon2an@users.noreply.github.com>
2021-09-18 10:54:11 +0700

    quiz

---
+++ quizIDON'TKNOW020.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.TreePlanting.Generic;
+namespace quizCode020
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input Total Rose : ");
+            Console.WriteLine("input Total Sun Flower : ");
+
+            Console.WriteLine("input Rose");
+            Console.WriteLine("---------- ");
+            Console.WriteLine("");
+            Console.WriteLine("ID : ");
+            Console.WriteLine("Plant name : ");
+            Console.WriteLine("Plant discription : ");
+            Console.WriteLine("Amount : ");
+            Console.WriteLine("Height : ");
+            Console.WriteLine("Circumference : ");
+
+            Console.WriteLine("input Rose");
+            Console.WriteLine("---------- ");
+            Console.WriteLine("");
+            Console.WriteLine("ID : ");
+            Console.WriteLine("Plant name : ");
+            Console.WriteLine("Plant discription : ");
+            Console.WriteLine("Amount : ");
+            Console.WriteLine("Height : ");
+            Console.WriteLine("Circumference : ");
+
+            Console.WriteLine("Rose Information");
+            Console.WriteLine("---------- ");
+
+            Console.WriteLine("Rose Information");
+            Console.WriteLine("---------- ");
+
+            //User jame = new User("jame watson", "jame@gmail.com");
+            //Tshirt shirt1 = new Tshirt("red", "L", "Monitor lizard", 500);
+            //Tshirt shirt2 = new Tshirt("black", "M", "FishStop", 750);
+            //Tshirt shirt3 = new Tshirt("rainbow", "S", "Prayut Chan-O-cha", 625);
+            //Address Ajame = new Address("Thanon Phutthamonthon", "Nakon Prathom", "10180");
+
+            //jame.Sent(); jame.shirt(shirt1); jame.shirt(shirt2); jame.shirt(shirt3);
+
+            //shirt1.OptionalTshirt(); shirt2.OptionalTshirt(); shirt3.OptionalTshirt();
+
+            //jame.Address(Ajame); Ajame.OptionalAddress();
+
+            ////Console.WriteLine("total cost : {0} bath", shirt1.Price + shirt2.Price + shirt3.Price);
+            //jame.TotalCost();
+            Console.ReadLine();
+        }
+
+    }
+
+    class Sunflower
+    {
+        public string Pantname, PlantDiscription, Height, Circumference;
+
+        public void
+            {
+
+            }
+    }
+    //class User
+    //{
+    //    public string UserName, @Email;
+
+    //    public List<Tshirt> OrderTShirst; public List<Address> Location;
+
+    //    public User(string name, string email)
+    //    {
+    //        UserName = name; @Email = email;
+    //        OrderTShirst = new List<Tshirt>();
+    //        Location = new List<Address>();
+    //    }
+
+    //    public void shirt(Tshirt tshirt)
+    //    { OrderTShirst.Add(tshirt); }
+    //    public void Address(Address Address)
+    //    { Location.Add(Address); }
+    //    public void Sent()
+    //    {
+    //        Console.WriteLine("Username : " + this.UserName);
+    //        Console.WriteLine("Email : " + this.@Email);
+    //        Console.WriteLine();
+    //    }
+
+    //    public void TotalCost()
+    //    {
+    //        float total = 0;
+    //        foreach (Tshirt tshirt in OrderTShirst)
+    //        { total = total + tshirt.Price; }
+    //        Console.WriteLine("Total cost : " + total + " Bath");
+    //    }
+    //}
+
+    //class Tshirt
+    //{
+    //    public string Size, Color, Image; public float Price;
+
+    //    public Tshirt(string color, string size, string image, float priec)
+    //    { Size = size; Color = color; Price = priec; Image = image; }
+
+    //    public void OptionalTshirt()
+    //    {
+    //        Console.WriteLine("**********************");
+    //        Console.WriteLine("Size : " + this.Size);
+    //        Console.WriteLine("Color : " + this.Color);
+    //        Console.WriteLine("Priec : " + this.Price);
+    //        Console.WriteLine("Image : " + this.Image);
+    //        Console.WriteLine("**********************");
+    //        Console.WriteLine();
+
+    //    }
+    //}
+
+    //class Address
+    //{
+    //    public string Street, City, Zipcode;
+
+    //    public Address(string street, string city, string zipcode)
+    //    { Street = street; City = city; Zipcode = zipcode; }
+
+    //    public void OptionalAddress()
+    //    { Console.WriteLine("131/75 street : " + this.Street + " ,city : " + this.City + " ,zipcode : " + this.Zipcode); }
+    //}
+
+    //class ShoppingCart
+    //{
+    //    public List<Tshirt> tshirt; public List<Address> addresses;
+
+    //    public ShoppingCart()
+    //    { tshirt = new List<Tshirt>(); addresses = new List<Address>(); }
+    //}
+}
